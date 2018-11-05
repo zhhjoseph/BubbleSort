5c5679712e80ef520dfa92603ad4a8cf38612e6c
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-05 12:30:09 -0500

    bubblesort implemented

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,6 +1,13 @@
 describe('Bubble Sort', function () {
+  const array1 = [13, 4, 89, 26, 57, 10, 31];
+  const array2 = [2, 6, 1, 15, 4, 12, 9];
+
+  beforeAll(function () {
+    spyOn(swap, 'call').and.callThrough();
+  });
+
   it('handles an empty array', function () {
-    expect(bubbleSort([13, 4, 89, 26, 57, 10, 31])).toEqual([4, 10, 13, 26, 31, 57, 89]);
-    expect(bubbleSort([2, 6, 1, 15, 4, 12, 9])).toEqual([1, 2, 4, 6, 9, 12, 15]);
+    expect(bubbleSort(array1)).toEqual([4, 10, 13, 26, 31, 57, 89]);
+    expect(bubbleSort(array2)).toEqual([1, 2, 4, 6, 9, 12, 15]);
   });
 });
